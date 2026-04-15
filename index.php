a7af7bb6f499008bbe7ebc23cef223860da5c9d4
Rian sebastian <riansebastian354@gmail.com>
2026-04-15 10:46:20 +0700

    Feat: Fitur menampilkan daftar laporan dalam tabel

--- index.php
+++ index.php
@@ -19,6 +19,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // tampilkan pesan sukses
     echo "<div style='color: green;'><b>Sukses!</b>Laporan dari<b>$nama</b>telah diterima: <i>$laporan</i></div><hr>";
 }
+// proses ambil data dari database
+$ambil_data =mysqli_query($conn, "SELECT * FROM pengaduan ORDER BY id DESC");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,10 +28,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Pengaduan -Helpdesk</title>
+    <style>
+        table { width: 100%; border-collapse: collapse; collapse: margin-top:20px; }
+        th, td { border: 1px solid #ddd; padding: 10px; text-align: left; }
+    </style>
 </head>
 <body>
     <h1>Kirim Lapotan Pengaduan</h1>
-    <form>
+    <form method="POST" action="">
         <label>Nama anda</label><br>
         <input type="text" name="nama" placeholder="Masukan nama... "><br><br>
 
@@ -37,7 +43,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <textarea name="laporan" placeholder="Tulis kendala Anda di sini..."></textarea><br><br>
 
         <button type="submit">Kirim Sekarang</button>
-
     </form>
+    <hr>
+    <h2>Daftar Laporan Masuk</h2>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama Pelapor</th>
+            <th>Laporan</th>
+        </tr>
+        <?php 
+        $no = 1; 
+        while($row = mysqli_fetch_array($ambil_data)){
+        ?>
+        <tr>
+            <td><?php echo $no++; ?></td>
+            <td><?php echo $row['nama']; ?></td>
+            <td><?php echo $row['laporan']; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
