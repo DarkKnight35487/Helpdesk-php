6b1f144b7ba706d5715ca19e10f60514585b0533
Rian sebastian <riansebastian354@gmail.com>
2026-04-15 10:32:03 +0700

    Feat: Fitur simpan laporan ke database

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
+include "koneksi.php";
 // cek apakah tombol kirim sudah ditekan
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     // ambil data dari form
-    $nama = $_POST['nama'];
-    $laporan = $_POST['laporan'];
+    $nama = $_POST["nama"];
+    $laporan = $_POST["laporan"];
+
+    $query = "INSERT INTO pengaduan (nama, laporan) VALUES ('$nama', '$laporan')";
+
+    if (mysqli_query($conn, $query)) {
+        echo "<div style='color: green;'><b>Sukses!</b>Laporan dari<b>$nama</b>telah diterima: <i>$laporan</i></div><hr>";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    }
     // simpan data ke database atau lakukan proses lainnya
+    // ...
+
     // tampilkan pesan sukses
-    echo "<div style='color: green;'><b>Sukses!</b> Laporan dari <b>$nama</b> telah diterima: <i>$laporan</i></div><hr>";
+    echo "<div style='color: green;'><b>Sukses!</b>Laporan dari<b>$nama</b>telah diterima: <i>$laporan</i></div><hr>";
 }
 ?>
 <!DOCTYPE html>
@@ -14,18 +25,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Pengaduan - Helpdesk</title>
+    <title>Sistem Pengaduan -Helpdesk</title>
 </head>
 <body>
-    <h1>Kirim Laporan Pengaduan</h1>
+    <h1>Kirim Lapotan Pengaduan</h1>
     <form>
-        <label>Nama Anda:</label>
-        <input type="text" name="nama" placeholder="Masukkan nama..."><br><br>
+        <label>Nama anda</label><br>
+        <input type="text" name="nama" placeholder="Masukan nama... "><br><br>
 
-        <label>Isi Laporan:</label><br>
-        <textarea name="laporan" placeholder ="Tuliskan kendala Anda di sini..."></textarea><br><br>
+        <label>Laporan</label><br>
+        <textarea name="laporan" placeholder="Tulis kendala Anda di sini..."></textarea><br><br>
 
         <button type="submit">Kirim Sekarang</button>
+
     </form>
 </body>
 </html>
